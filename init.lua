9591ae7ca2bf14b43c6c8f3bfb90d4b3b4fdef38
Sanghyun Lee <hammerha@gmail.com>
2021-11-20 23:21:01 +0100

    Add reload shortcut

--- init.lua
+++ init.lua
@@ -1,10 +1,13 @@
 -- https://www.hammerspoon.org/go/#smartreload
-hs.loadSpoon("ReloadConfiguration")
+hs.loadSpoon('ReloadConfiguration')
 spoon.ReloadConfiguration:start()
 
-function hello()
-  hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
-end
+-- Sometimes it's needed to force reload
+-- for example when Redshift doesn't work for a specific screen
+-- (a similar issue https://github.com/Hammerspoon/hammerspoon/issues/1197)
+hs.hotkey.bind({'ctrl', 'shift', 'cmd', 'alt'}, 'h', function()
+  hs.reload()
+end)
 
 require('redshift')
 require('finder')
