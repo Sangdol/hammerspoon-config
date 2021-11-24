0be35310e1106f51bad538ce98f1c5f818087d95
Sanghyun Lee <hammerha@gmail.com>
2021-11-24 22:23:59 +0100

    Add shortcut reference

--- window_shortcuts.lua
+++ window_shortcuts.lua
@@ -9,3 +9,11 @@ hs.hotkey.bind({"ctrl", "shift", "cmd"}, "j", wl.moveWindowTo(-1))
 hs.hotkey.bind({"ctrl", "shift", "cmd"}, "left", wl.moveFocusedWindowToLeft)
 hs.hotkey.bind({"ctrl", "shift", "cmd"}, "right", wl.moveFocusedWindowToRight)
 hs.hotkey.bind({"ctrl", "cmd"}, "f", wl.fullscreenCurrent)
+
+-- Reference
+-- - This doesn't make app changing faster.
+-- - This doesn't focus back to the previously used window.
+-- => basically it's the same as BTT
+--hs.hotkey.bind({"ctrl", "cmd"}, "0", function()
+  --hs.application.launchOrFocus('iTerm')
+--end)
