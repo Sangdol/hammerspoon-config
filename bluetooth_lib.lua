ac6dc42ea08f723d42af9da4eafebcdb3239091a
Sanghyun Lee <hammerha@gmail.com>
2021-12-07 21:34:19 +0100

    Add comment bluetooth script

--- bluetooth_lib.lua
+++ bluetooth_lib.lua
@@ -2,6 +2,7 @@ bt = {}
 
 function bt.connect()
   logger:d("Connecting to bluetooth")
+  -- bluetooth.sh: a script to connect to a specific device using blueutil
   hs.execute('$HOME/projects/osx/scripts/bluetooth.sh', true)
 end
 
