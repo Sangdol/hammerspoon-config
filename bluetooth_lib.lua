66fee972cb50d209babbb747b9da1d4fe6d590b1
Sanghyun Lee <hammerha@gmail.com>
2021-12-19 13:18:26 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
    ddddddddddd
    
    # Please enter the commit message for your changes. Lines starting
    # with ';' will be ignored, and an empty message aborts the commit.
    #
    # On branch master
    # Your branch is ahead of 'origin/master' by 4 commits.
    #   (use "git push" to publish your local commits)
    #
    # Changes to be committed:
    #	modified:   bluetooth_lib.lua
    #	modified:   notification_lib.lua
    #
    # ------------------------ >8 ------------------------
    # Do not modify or remove the line above.
    # Everything below it will be ignored.
    diff --git a/bluetooth_lib.lua b/bluetooth_lib.lua
    index 584613d..a67a1fe 100644
    --- a/bluetooth_lib.lua
    +++ b/bluetooth_lib.lua
    @@ -1,20 +1,25 @@
     bt = {}
    
    +local logger = hs.logger.new('BT_lib', 'debug')
    +
     function bt.connect()
    -  logger:d("Connecting to bluetooth")
    +  logger:i("Connecting to bluetooth")
       -- bluetooth.sh: a script to connect to a specific device using blueutil
       hs.execute('$HOME/projects/osx/scripts/bluetooth.sh', true)
     end
    
     function bt.conditionallyConnect()
       -- The number of connected screens can be also used.
    -  if hs.battery.isCharging() then
    +  if hs.battery.powerSource() == 'AC Power' then
    +    logger:d('AC Power and connecting to BT')
         bt.connect()
    +  else
    +    logger:d('No AC Power and skipping BT')
       end
     end
    
     function bt.turnOff()
    -  logger:d("Turning off bluetooth")
    +  logger:i("Turning off bluetooth")
       hs.execute('/usr/local/bin/blueutil --power 0', true)
     end
    
    diff --git a/notification_lib.lua b/notification_lib.lua
    index 19891c5..c889bfc 100644
    --- a/notification_lib.lua
    +++ b/notification_lib.lua
    @@ -5,11 +5,11 @@
     no = {}
    
     function no.alert(msg)
    -  hs.alert.show(msg)
    +  return hs.alert.show(msg)
     end
    
     function no.notify(msg)
    -  hs.notify.new({title="Hammerspoon", informativeText=msg}):send()
    +  return hs.notify.new({title="Hammerspoon", informativeText=msg}):send()
     end
    
     return no

--- bluetooth_lib.lua
+++ bluetooth_lib.lua
@@ -1,20 +1,25 @@
 bt = {}
 
+local logger = hs.logger.new('BT_lib', 'debug')
+
 function bt.connect()
-  logger:d("Connecting to bluetooth")
+  logger:i("Connecting to bluetooth")
   -- bluetooth.sh: a script to connect to a specific device using blueutil
   hs.execute('$HOME/projects/osx/scripts/bluetooth.sh', true)
 end
 
 function bt.conditionallyConnect()
   -- The number of connected screens can be also used.
-  if hs.battery.isCharging() then
+  if hs.battery.powerSource() == 'AC Power' then
+    logger:d('AC Power and connecting to BT')
     bt.connect()
+  else
+    logger:d('No AC Power and skipping BT')
   end
 end
 
 function bt.turnOff()
-  logger:d("Turning off bluetooth")
+  logger:i("Turning off bluetooth")
   hs.execute('/usr/local/bin/blueutil --power 0', true)
 end
 
--- notification_lib.lua
+++ notification_lib.lua
@@ -5,11 +5,11 @@
 no = {}
 
 function no.alert(msg)
-  hs.alert.show(msg)
+  return hs.alert.show(msg)
 end
 
 function no.notify(msg)
-  hs.notify.new({title="Hammerspoon", informativeText=msg}):send()
+  return hs.notify.new({title="Hammerspoon", informativeText=msg}):send()
 end
 
 return no
