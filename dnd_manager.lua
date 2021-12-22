9ca1d0daedb2283cd535318ca91dda4a7a46bab5
Sanghyun Lee <hammerha@gmail.com>
2021-12-22 17:30:57 +0100

    Refactor dnd_manager

--- dnd_manager.lua
+++ dnd_manager.lua
@@ -9,6 +9,7 @@ no = require('notification_lib')
 dnd = {}
 
 local logger = hs.logger.new('dnd_manager', 5)
+local DND_JS = '/usr/local/bin/node $HOME/projects/do-not-disturb-timer-jxa/onoff.js'
 
 apps = {'Melodics', 'Movist'}
 
@@ -20,26 +21,23 @@ function dnd.appWatcherHandler(appName, eventType, appObject)
   if (eventType == hs.application.watcher.launched) and
     (tl.isInList(apps, appName)) then
     logger:d('DND On', appName)
-    dnd.onOff(true)
+    dnd.turnOn()
     bt.turnOff()
   elseif (eventType == hs.application.watcher.terminated) then
     logger:d('DND Off', appName)
-    dnd.onOff(false)
+    dnd.turnOff()
     bt.conditionallyConnect()
   end
 end
 
-function dnd.onOff(on)
-  -- https://github.com/Sangdol/do-not-disturb-timer-jxa
-  local prefix = '/usr/local/bin/node $HOME/projects/do-not-disturb-timer-jxa/onoff.js'
+function dnd.turnOn()
+  no.alert('DND On')
+  hs.execute(DND_JS .. ' ' .. 'on', true)
+end
 
-  if (on) then
-    no.alert('DND On')
-    hs.execute(prefix .. ' ' .. 'on', true)
-  else
-    no.alert('DND Off')
-    hs.execute(prefix .. ' ' .. 'off', true)
-  end
+function dnd.turnOff()
+  no.alert('DND Off')
+  hs.execute(DND_JS .. ' ' .. 'off', true)
 end
 
 dnd.appWatcher = hs.application.watcher.new(dnd.appWatcherHandler)
