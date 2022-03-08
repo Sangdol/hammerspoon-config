3178c7d8c6d971b9754bf0bdbe6674fced880bce
Sanghyun Lee <hammerha@gmail.com>
2022-03-08 18:40:34 +0100

    Remove unused code

--- dnd.lua
+++ dnd.lua
@@ -5,13 +5,10 @@
 local dnd = require('lib/dnd_lib')
 local bt = require('lib/bluetooth_lib')
 local tl = require('lib/table_lib')
-local st = require('lib/string_lib')
-
-local logger = hs.logger.new('dnd_manager', 5)
 
 local dndApps = {'Melodics', 'Movist'}
 
-local function appWatcherHandler(appName, eventType, appObject)
+local function appWatcherHandler(appName, eventType)
   if (not tl.isInList(dndApps, appName)) then
     return
   end
