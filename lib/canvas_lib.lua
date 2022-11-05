139db018d5433a475b7bf87fc90a37536d04a51c
Sanghyun Lee <hammerha@gmail.com>
2022-11-05 10:13:51 +0100

    Refactoring: remove duplicate attribute

--- lib/canvas_lib.lua
+++ lib/canvas_lib.lua
@@ -9,7 +9,6 @@ function ca.showItermSign(frame)
       action = "fill",
       fillColor = { alpha = 0.5, green = 0.5, red = 0.4, blue = 0.3 },
       frame = { x = "0", y = "0", h = "0.01", w = "1", },
-      type = "rectangle",
     }):show()
 end
 
