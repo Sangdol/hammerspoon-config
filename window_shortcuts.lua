175daa944ac4820b09aeab599b9a2c173336b69e
Sanghyun Lee <hammerha@gmail.com>
2022-11-20 20:36:42 +0100

    Fix wrong edit

--- window_shortcuts.lua
+++ window_shortcuts.lua
@@ -67,4 +67,3 @@ end
 hs.hotkey.bind({"ctrl", "cmd"}, "J", ws.selectLastActiveWindow('Google Chrome'))
 hs.hotkey.bind({"ctrl", "cmd"}, "C", ws.selectLastActiveWindow('Google Chrome Canary'))
 hs.hotkey.bind({"ctrl", "cmd"}, "U", ws.selectLastActiveWindow('Firefox'))
-hs.hotkey.bind({"ctrl", "cmd"}, "J", ws.selectLastActiveWindow('Brave Browser'))
