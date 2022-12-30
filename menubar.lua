46384634023e78d6b49ff3497c12d8100c419534
Sanghyun Lee <hammerha@gmail.com>
2022-12-30 09:19:08 +0100

    Hide menubar items when no message / add more items (youtube / mail)

--- menubar.lua
+++ menubar.lua
@@ -7,17 +7,44 @@ local st = require('lib/string_lib')
 local PATH_PREFIX = '$HOME/projects/osx/textbar'
 local UPDATE_INTERVAL = 5
 
---local mailMenu = hs.menubar.new():setTitle('Mail…')
-local pomoMenu = hs.menubar.new():setTitle('Pomo…')
-local teaMenu = hs.menubar.new():setTitle('Tea…')
-
 local function executeScript(scriptName)
   return st.trim(hs.execute(PATH_PREFIX .. '/' .. scriptName))
 end
 
+local menubar = {
+  _items = {},
+  _add = function(self, name, title, fn)
+    local item = hs.menubar.new():setTitle(title)
+    item:setClickCallback(fn)
+    self._items[name] = item
+  end,
+  set = function(self, name, title, fn)
+    if self._items[name] then
+      self._items[name]:setTitle(title)
+    else
+      self._add(self, name, title, fn)
+    end
+  end,
+  delete = function(self, name)
+    if self._items[name] then
+      self._items[name]:delete()
+      self._items[name] = nil
+    end
+  end,
+}
+
 -- This has to be global not to be garbage collected.
 MenubarTimer = hs.timer.new(UPDATE_INTERVAL, function()
-  pomoMenu:setTitle(executeScript('pomo.sh'))
-  teaMenu:setTitle(executeScript('tea.sh'))
+  local scripts = {'pomo', 'tea', 'mail-checker', 'youtube-music'}
+
+  for _, scriptname in ipairs(scripts) do
+    local result = executeScript(scriptname .. '.sh')
+
+    if result ~= '' then
+      menubar:set(scriptname, result)
+    else
+      menubar:delete(scriptname)
+    end
+  end
 end):start()
 
