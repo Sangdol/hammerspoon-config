7bfa72507e4428a3087ff1b742e8ca55bd72c686
Sanghyun Lee <hammerha@gmail.com>
2023-03-02 21:13:04 +0100

    Don't start DND during working hours

--- dnd.lua
+++ dnd.lua
@@ -5,8 +5,14 @@
 local dndApps = {'IINA'}
 local logger = hs.logger.new('dnd', 'debug')
 
+local function isWorkingHours()
+  local hour = tonumber(os.date('%H'))
+  local day = tonumber(os.date('%w'))
+  return (hour >= 9) and (hour < 18) and (day ~= 0) and (day ~= 6)
+end
+
 local function appWatcherHandler(appName, eventType)
-  if (not tl.isInList(dndApps, appName)) then
+  if (not tl.isInList(dndApps, appName)) and isWorkingHours() then
     return
   else 
     logger:d('DND is starting. App Name: ', appName)
