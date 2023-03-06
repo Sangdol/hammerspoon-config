d63863558ead80acd99b94a68390a44ebcc72ec8
Sanghyun Lee <hammerha@gmail.com>
2023-03-06 21:40:12 +0100

    Refactoring: expose window_focus object
    To be able to test it

--- window_focus.lua
+++ window_focus.lua
@@ -5,7 +5,7 @@
 
 local logger = hs.logger.new('window_focus', 'info')
 
-local ws = {}
+Wf = {}
 
 hs.hotkey.bind({"ctrl", "shift", "cmd"}, "l", sc.currentWindowCenterToggle)
 hs.hotkey.bind({"ctrl", "shift", "cmd"}, "j", wl.moveFocusedWindowToLeft)
@@ -23,14 +23,14 @@ hs.hotkey.bind({"ctrl", "cmd"}, "7", wl.resizeAndCenterCurrent(0.8))
 --
 -- Shortcuts to focus the last used window
 --
-ws.lastUsedWins = {}
+Wf.lastUsedWins = {}
 
 -- Set the last used window when a window is unfocused.
 -- It sets the window when unfocused instead of focused
 -- to make it run after the applicatino watcher.
 hs.window.filter.default:subscribe(hs.window.filter.windowUnfocused, function(win, appName)
   logger:d(appName)
-  ws.lastUsedWins[appName] = win
+  Wf.lastUsedWins[appName] = win
   logger:d(appName, 'window updated')
 end)
 
@@ -42,7 +42,7 @@ AppFocusWatcher = hs.application.watcher.new(function(appName, eventType)
       -- Do not use it for iTerm2 for now.
       return
     end
-    ws.selectLastActiveWindow(appName)()
+    Wf.selectLastActiveWindow(appName)()
   end
 end)
 AppFocusWatcher:start()
@@ -51,10 +51,10 @@ AppFocusWatcher:start()
 -- and the appName used in subscribe can be different
 -- for example iTerm for `launchOrFocus()` and iTerm2 for `subscribe()`.
 -- Due to this, this function can't work for a not running application.
-function ws.selectLastActiveWindow(appName)
+function Wf.selectLastActiveWindow(appName)
   local function selectApp()
-    if (ws.lastUsedWins[appName]) then
-      local win = ws.lastUsedWins[appName]
+    if (Wf.lastUsedWins[appName]) then
+      local win = Wf.lastUsedWins[appName]
 
       -- A hack to focus the last used window.
       -- https://github.com/Hammerspoon/hammerspoon/issues/370#issuecomment-615535897
@@ -76,9 +76,9 @@ function ws.selectLastActiveWindow(appName)
   return selectApp
 end
 
-hs.hotkey.bind({"ctrl", "cmd"}, "J", ws.selectLastActiveWindow('Google Chrome'))
-hs.hotkey.bind({"ctrl", "cmd"}, "C", ws.selectLastActiveWindow('Google Chrome Canary'))
-hs.hotkey.bind({"ctrl", "cmd"}, "U", ws.selectLastActiveWindow('Firefox'))
-hs.hotkey.bind({"ctrl", "cmd"}, ";", ws.selectLastActiveWindow('Microsoft Edge'))
-hs.hotkey.bind({"ctrl", "cmd"}, "I", ws.selectLastActiveWindow('Anki'))
+hs.hotkey.bind({"ctrl", "cmd"}, "J", Wf.selectLastActiveWindow('Google Chrome'))
+hs.hotkey.bind({"ctrl", "cmd"}, "C", Wf.selectLastActiveWindow('Google Chrome Canary'))
+hs.hotkey.bind({"ctrl", "cmd"}, "U", Wf.selectLastActiveWindow('Firefox'))
+hs.hotkey.bind({"ctrl", "cmd"}, ";", Wf.selectLastActiveWindow('Microsoft Edge'))
+hs.hotkey.bind({"ctrl", "cmd"}, "I", Wf.selectLastActiveWindow('Anki'))
 
