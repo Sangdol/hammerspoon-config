194431bb32a024e9e71598a437808e0e1b9d5dcc
Sanghyun Lee <hammerha@gmail.com>
2023-03-14 21:38:27 +0100

    Move cursor to focused window screen

--- cursor.lua
+++ cursor.lua
@@ -2,11 +2,11 @@
 -- Show me the cursor!
 --
 
-local DIAMETER = 60
-local STROKE_WIDTH = 2
-local DURATION = 1
+local DIAMETER = 40
+local STROKE_WIDTH = 1
+local DURATION = 0.5
 local COLOR = '#603b5a'
-local ALPHA = 0.4
+local ALPHA = 0.3
 
 local function mouseHighlight()
   local mousepoint = hs.mouse.absolutePosition()
@@ -29,14 +29,14 @@ local function moveCursorToCenter()
   hs.mouse.absolutePosition(center)
 end
 
-local function main()
+local function centerCursor()
   moveCursorToCenter()
   mouseHighlight()
 end
 
 -- Move cursor to the center of current screen
-hs.hotkey.bind({"ctrl","alt"}, "c", main)
-hs.hotkey.bind({"ctrl","shift"}, "c", main)
+hs.hotkey.bind({"ctrl","alt"}, "c", centerCursor)
+hs.hotkey.bind({"ctrl","shift"}, "c", centerCursor)
 
 -- This function finds the next screen based on the current mouse position.
 local function moveCursorToNextScreen(direction)
@@ -53,3 +53,13 @@ end
 -- Move cursor to the center of next screen
 hs.hotkey.bind({"ctrl","alt"}, "n", moveCursorToNextScreen(-1))
 hs.hotkey.bind({"ctrl","alt"}, "m", moveCursorToNextScreen(1))
+
+-- Move cursor to the focused window screen when an application window is activated
+-- if the cursor is not on the focused window screen.
+hs.window.filter.default:subscribe(hs.window.filter.windowFocused, function()
+  local currentScreen = hs.mouse.getCurrentScreen()
+  local focusedScreen = hs.window.focusedWindow():screen()
+  if currentScreen ~= focusedScreen then
+    centerCursor()
+  end
+end)
