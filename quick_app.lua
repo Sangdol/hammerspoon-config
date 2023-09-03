8b30aa319a43eea7e20000006be16d1366604df4
Sanghyun Lee <hammerha@gmail.com>
2023-09-03 12:50:32 +0200

    Good by active-app-memory-jxa

---
+++ quick_app.lua
@@ -0,0 +1,22 @@
+--
+-- Quick app switcher
+--
+local activeApp = nil
+
+-- Bind Ctrl + Shift + Cmd + H to store the quick app
+hs.hotkey.bind({"ctrl", "shift", "cmd"}, "H", function()
+  local focusedWindow = hs.window.focusedWindow()
+  if focusedWindow then
+    activeApp = focusedWindow:application()
+    no.alert('Stored quick app: ' .. activeApp:name())
+  end
+end)
+
+-- Bind Ctrl + Cmd + H to focus the stored quick app
+hs.hotkey.bind({"ctrl", "cmd"}, "H", function()
+  if activeApp then
+    activeApp:activate()
+  else
+    no.alert('No quick app stored')
+  end
+end)
