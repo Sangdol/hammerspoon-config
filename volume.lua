e2872bc367c239e9d5c45b8de1fb1756b7168ede
Sanghyun Lee <hammerha@gmail.com>
2023-10-28 20:46:24 +0200

    Remove volume control shortcuts after 50%

--- volume.lua
+++ volume.lua
@@ -34,11 +34,6 @@ hs.hotkey.bind({"ctrl", "shift"}, "2", function() setVolume(20) end)
 hs.hotkey.bind({"ctrl", "shift"}, "3", function() setVolume(30) end)
 hs.hotkey.bind({"ctrl", "shift"}, "4", function() setVolume(40) end)
 hs.hotkey.bind({"ctrl", "shift"}, "5", function() setVolume(50) end)
-hs.hotkey.bind({"ctrl", "shift"}, "6", function() setVolume(60) end)
-hs.hotkey.bind({"ctrl", "shift"}, "7", function() setVolume(70) end)
-hs.hotkey.bind({"ctrl", "shift"}, "8", function() setVolume(80) end)
-hs.hotkey.bind({"ctrl", "shift"}, "9", function() setVolume(90) end)
-hs.hotkey.bind({"ctrl", "shift"}, "0", function() setVolume(100) end)
 
 -- Hotkey definitions for volume change
 hs.hotkey.bind({"ctrl", "alt"}, "-", function() changeVolume(-3) end)
