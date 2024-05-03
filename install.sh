71db94015d858a80bedbe13e8ccd093dc87bc606
Sanghyun Lee <hammerha@gmail.com>
2024-05-03 20:36:20 +0200

    improve install.sh script

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+# Without this, the ln command will create a symlink inside the ~/.hammerspoon directory
+if [ -d ~/.hammerspoon ]; then
+  echo "Moving existing ~/.hammerspoon to trash"
+  trash ~/.hammerspoon
+fi
+
 ln -sfvn ~/projects/hammerspoon-config ~/.hammerspoon
