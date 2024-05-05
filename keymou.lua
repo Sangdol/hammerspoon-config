dd702f51ca08d1a90ace5e8a8681961f4e307d6e
Sanghyun Lee <hammerha@gmail.com>
2024-05-05 11:56:11 +0200

    Add cursor movement shortcuts to ditch Keymou

--- keymou.lua
+++ keymou.lua
@@ -67,3 +67,65 @@ hs.hotkey.bind({"ctrl", "alt"}, "K", scrollUp, nil, scrollUp)
 hs.hotkey.bind({"ctrl", "alt"}, "J", scrollDown, nil, scrollDown)
 hs.hotkey.bind({"ctrl", "alt"}, "H", scrollLeft, nil, scrollLeft)
 hs.hotkey.bind({"ctrl", "alt"}, "L", scrollRight, nil, scrollRight)
+
+
+-- 
+-- Cursor movement (fast)
+--
+local cursorMods = {"ctrl", "shift"}
+local cursorDistance = 120
+
+local function moveCursor(dx, dy)
+    local currentPos = hs.mouse.getAbsolutePosition()
+    local newPos = hs.geometry.point(currentPos.x + dx, currentPos.y + dy)
+    hs.mouse.setAbsolutePosition(newPos)
+end
+
+local moveUp = function()
+    moveCursor(0, -cursorDistance)
+end
+
+local moveDown = function()
+    moveCursor(0, cursorDistance)
+end
+
+local moveLeft = function()
+    moveCursor(-cursorDistance, 0)
+end
+
+local moveRight = function()
+    moveCursor(cursorDistance, 0)
+end
+
+hs.hotkey.bind(cursorMods, "K", moveUp, nil, moveUp)
+hs.hotkey.bind(cursorMods, "J", moveDown, nil, moveDown)
+hs.hotkey.bind(cursorMods, "H", moveLeft, nil, moveLeft)
+hs.hotkey.bind(cursorMods, "L", moveRight, nil, moveRight)
+
+--
+-- Cursor movement (slow)
+--
+local slowCursorMods = {"ctrl", "shift", "alt"}
+local slowCursorDistance = 15
+
+local slowMoveUp = function()
+    moveCursor(0, -slowCursorDistance)
+end
+
+local slowMoveDown = function()
+    moveCursor(0, slowCursorDistance)
+end
+
+local slowMoveLeft = function()
+    moveCursor(-slowCursorDistance, 0)
+end
+
+local slowMoveRight = function()
+    moveCursor(slowCursorDistance, 0)
+end
+
+hs.hotkey.bind(slowCursorMods, "K", slowMoveUp, nil, slowMoveUp)
+hs.hotkey.bind(slowCursorMods, "J", slowMoveDown, nil, slowMoveDown)
+hs.hotkey.bind(slowCursorMods, "H", slowMoveLeft, nil, slowMoveLeft)
+hs.hotkey.bind(slowCursorMods, "L", slowMoveRight, nil, slowMoveRight)
+
