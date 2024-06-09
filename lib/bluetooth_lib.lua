77713b2b53ac4fa2de6ec46c66c62f26f8030598
Sanghyun Lee <hammerha@gmail.com>
2024-06-09 20:48:09 +0200

    Add comments

--- lib/bluetooth_lib.lua
+++ lib/bluetooth_lib.lua
@@ -14,10 +14,13 @@ function bt.connect()
   hs.execute('$HOME/projects/osx/scripts/bluetooth.sh', true)
 end
 
+-- This can be used to connect to a bluetooth device only when the laptop is connected to AC Power.
+-- This is useful when you want to use a bluetooth keyboard and mouse only when you are at your desk.
 function bt.conditionallyConnect()
   -- The number of connected screens can be also used.
   if hs.battery.powerSource() == 'AC Power' then
     logger:d('AC Power and connecting to BT')
+    -- What's the latency of this?
     bt.connect()
   else
     logger:d('No AC Power and skipping BT')
