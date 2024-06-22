ed16572aff2adee62f5cd8fda0c72653630edc01
Sanghyun Lee <hammerha@gmail.com>
2024-06-22 22:05:04 +0200

    Store quick app in a file

--- quick_app.lua
+++ quick_app.lua
@@ -6,19 +6,50 @@
 -- For quick app switcher
 --
 local logger = hs.logger.new('quick_app.lua', 'info')
-local activeApp = nil
+local home = os.getenv('HOME')
+local fileAppName = {
+  path = home .. '/.sang_storage/hammerspoon_quick_app_name',
+  read = function(self)
+    local file = io.open(self.path, 'r')
+    if file then
+      local name = file:read()
+      file:close()
+      return name
+    else
+      return nil
+    end
+  end,
+  write = function(self, name)
+    local file = io.open(self.path, 'w')
+    if not file then
+      logger:e('Failed to open the file to write the app name')
+      return
+    end
+    file:write(name)
+    file:close()
+  end
+}
 
 -- Bind Ctrl + Shift + Cmd + H to store the quick app
 hs.hotkey.bind({"ctrl", "shift", "cmd"}, "P", function()
   local focusedWindow = hs.window.focusedWindow()
   if focusedWindow then
-    activeApp = focusedWindow:application()
+    local activeApp = focusedWindow:application()
+    if not activeApp then
+      no.alert('No active app')
+      return
+    end
+
+    fileAppName:write(activeApp:name())
     no.alert('Stored quick app: ' .. activeApp:name())
   end
 end)
 
 -- Bind Ctrl + Cmd + H to focus the stored quick app
 hs.hotkey.bind({"ctrl", "cmd"}, "P", function()
+  local activeAppName = fileAppName:read()
+  print(activeAppName)
+  local activeApp = hs.application.find(activeAppName)
   if activeApp then
     activeApp:activate()
   else
