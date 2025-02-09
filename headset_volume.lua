386e49eb20c9a9098ad94332ed9223f08a1d6c0b
Sanghyun Lee <hammerha@gmail.com>
2025-02-09 13:05:18 +0100

    Refactor headset_volume

--- headset_volume.lua
+++ headset_volume.lua
@@ -16,43 +16,44 @@ local function applyStoredVolume()
   logger:d('Applying stored volume')
 
   local file = io.open(volumeFile, "r")
-  if file then
-    local storedVol = tonumber(file:read("*a"))
-    file:close()
-    if storedVol then
-      local targetVol = math.min(storedVol, maxVolume)
-      local currentDevice = audio.defaultOutputDevice()
-      if currentDevice and string.find(currentDevice:name(), headsetName) then
-        currentDevice:setVolume(targetVol)
-        hs.alert("Volume set to " .. targetVol .. "%")
-      end
-    end
-  else
-    -- If no stored volume, set to maxVolume and save
-    local currentDevice = audio.defaultOutputDevice()
-    if currentDevice and string.find(currentDevice:name(), headsetName) then
-      currentDevice:setVolume(maxVolume)
-      hs.execute(string.format("echo '%d' > %s", maxVolume, volumeFile))
-    end
-  end
+  if not file then return end
+
+  local storedVol = tonumber(file:read("*a"))
+  file:close()
+
+  if not storedVol then return end
+
+  local targetVol = math.min(storedVol, maxVolume)
+  local currentDevice = audio.defaultOutputDevice()
+
+  if not currentDevice then return end
+  if not string.find(currentDevice:name(), headsetName) then return end
+
+  -- Only set volume if it's different from lastVolume
+  if targetVol == lastVolume then return end
+
+  currentDevice:setVolume(targetVol)
+  lastVolume = targetVol
+  hs.alert("Volume set to " .. targetVol .. "%")
 end
 
 -- Start monitoring volume changes for the headset
 local function startVolumeTimer()
   logger:d('Starting volume timer')
-
   if volumeTimer then return end
+
   volumeTimer = hs.timer.new(5, function()
     local currentDevice = audio.defaultOutputDevice()
-    if currentDevice and string.find(currentDevice:name(), headsetName) then
-      local currentVol = math.floor(currentDevice:volume())
-      logger:d('Current volume:', currentVol)
-      logger:d('Last volume:', lastVolume)
-      if currentVol ~= lastVolume then
-        lastVolume = currentVol
-        hs.execute(string.format("echo '%d' > %s", currentVol, volumeFile))
-      end
-    end
+    if not currentDevice then return end
+    if not string.find(currentDevice:name(), headsetName) then return end
+
+    local currentVol = math.floor(currentDevice:volume())
+    logger:d('Current volume:', currentVol)
+    logger:d('Last volume:', lastVolume)
+    if currentVol == lastVolume then return end
+
+    lastVolume = currentVol
+    hs.execute(string.format("echo '%d' > %s", currentVol, volumeFile))
   end)
   volumeTimer:start()
 end
